afcd4089b024a7bb05dd50130573a4113fa3b508
Yannis Ouldali <yannis.ouldali@spendesk.com>
2022-06-02 22:57:51 +0200

    update index.s

--- react-browser-search/src/index.ts
+++ react-browser-search/src/index.ts
@@ -2,5 +2,6 @@ export * from './useQuery';
 export * from './useIndexValues';
 export * from './useCreateStore';
 export * from './useAddDataToStore';
+export * from './useDeleteStore';
 export { BrowserSearchProvider } from './provider';
 export * from './queryState';
