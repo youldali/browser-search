04be34cd8aaa418aca902778387d94a01bd29e0e
Yannis Ouldali <yannis.ouldali@spendesk.com>
2022-06-02 17:26:46 +0200

    removed useMutateStore

--- react-browser-search/src/index.ts
+++ react-browser-search/src/index.ts
@@ -2,6 +2,5 @@ export * from './useQuery';
 export * from './useIndexValues';
 export * from './useCreateStore';
 export * from './useAddDataToStore';
-export { useMutateStore } from './useMutateStore';
 export { BrowserSearchProvider } from './provider';
 export * from './queryState';
